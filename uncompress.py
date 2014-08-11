e90da4e268d3d13edd384fde86aa913a75676e2d
Aoyama Nanami <aoyama_nanami@sakurasou>
2014-08-11 17:12:15 +0800

    a script for decompress .ab file

---
+++ uncompress.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+# a simple script for decompress .ab file
+
+import zlib
+
+tar = open('pad.tar', 'w')
+
+d = zlib.decompressobj()
+
+ab = open('pad.ab', 'r')
+s = ab.read(24)
+while True :
+    s = ab.read(1024 * 1024)
+    if s == "" : break
+    t = d.decompress(s)
+    tar.write(t)
+
+ab.close()
+tar.close()
+
